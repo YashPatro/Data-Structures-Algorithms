137df88b23b70699de6f96e7e6f8c00688c0509e
YashPatro <rashmita@247commerce.co.uk>
2025-08-23 17:11:41 +0100

    Add files via upload

--- oops.py
+++ oops.py
@@ -16,7 +16,7 @@ orange = fruit('Orange','sphere','sweet','China/India/Myanmar','orange')
 orange.printer()
 print(orange.taste)'''
 #python inheritence
-class person():
+'''class person():
     def __init__(self,name,age,birthplace):
         self.name = name
         self.age = age
@@ -37,3 +37,29 @@ employed = child('Yash','14','London','100,000/anum','COO')
 unemployed.printer()
 employed.printer2()
 employed.printer()
+
+'''
+#due 23/08/25 homework
+class teacher():
+    def __init__(self,name,subject,total_classes):
+        self.name = name
+        self.subject = subject
+        self.total_classes = total_classes
+
+    def printer(self):
+        print(f'{self.name} = name,{self.subject} = subject,{self.total_classes} = total classes being taught')
+
+class class1(teacher):
+    def __init__(self,name,subject,total_classes,name_class,num_students):
+        self.name = name
+        self.subject = subject
+        self.total_classes = total_classes
+        self.name_class = name_class
+        self.num_student = num_students
+
+    def printer(self):
+        print(f'{self.name} = name of teacher,{self.subject} = subject,{self.name_class} = name_class,{self.num_student} = number of students')
+
+mr_patro = teacher('yash','maths',5)
+class_10AB = class1('yash','maths',5,'10AB',31)
+class_10AB.printer() 
