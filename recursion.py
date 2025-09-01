e44aef057b7490867273ee4db205b09b646ad438
YashPatro <rashmita@247commerce.co.uk>
2025-09-01 15:22:49 +0100

    Add files via upload
    01/09/25

---
+++ recursion.py
@@ -0,0 +1,35 @@
+#01/09/25
+
+def factorial(num):
+    t =  1
+    for i in range(1,num+1):
+        t = t*i
+    print(t)
+
+factorial(10)
+
+
+def f_recursion(n):
+    if n == 1:
+        return 1
+    else:
+        return n*f_recursion(n-1)
+    
+def write(n):
+    if n == 1:
+        print('1\nHappy New Year!!!')
+    else:
+        print(n)
+        write(n-1)
+
+write(10)
+
+def items(l):
+    if not l:
+        return 0
+    else:
+        
+        return l[0]+items(l[1:])
+l1 = [1,2,3,4,5]
+
+print(items(l1))
